8b88781ef3e6ee3f07158403d727c1c604fcaf77
takaodaze <gin2798@gmail.com>
2021-07-14 09:21:19 +0900

    implementing use timer

--- src/components/PomodoroTimer/PomodoroTimer.tsx
+++ src/components/PomodoroTimer/PomodoroTimer.tsx
@@ -6,13 +6,15 @@ import { TimerMode } from "./types";
 const PomodoroTimer: FC<ClassNameProps> = ({ className }) => {
   const [timerMode] = useState<TimerMode>("work");
   // 秒で計算する
-  const [remainingTime, setRemainingTime, startTimer] = useTime(timerMode);
+  const { remainingTime, startTimer, modeChange } = useTime(timerMode);
 
   return (
     <div className={className}>
       <span>{remainingTime}</span>
       <br />
       <button onClick={() => startTimer()}>start!</button>
+      <br />
+      <button onClick={() => modeChange("rest")}>mode change!!!</button>
     </div>
   );
 };
--- src/components/PomodoroTimer/hooks/useTime.ts
+++ src/components/PomodoroTimer/hooks/useTime.ts
@@ -6,33 +6,60 @@ import {
 } from "../constants";
 import { TimerMode } from "../types";
 
-const useTime = (
-  mode: TimerMode
-): [number, (n: number) => void, () => void] => {
-  let initTime: number;
+type ReturnType = {
+  remainingTime: number;
+  startTimer: () => void;
+  modeChange: (mode: TimerMode) => void;
+};
+
+const calcInitTime = (mode: TimerMode) => {
   if (mode === "work") {
-    initTime = INIT_WORK_TIME;
+    return INIT_WORK_TIME;
   } else if (mode === "rest") {
-    initTime = INIT_REST_TIME;
+    return INIT_REST_TIME;
   } else if (mode === "longRest") {
-    initTime = INIT_LONG_REST_TIME;
+    return INIT_LONG_REST_TIME;
   } else {
     throw new Error(`invalid mode:${mode}`);
   }
+};
 
-  const [remainingTime, setRemainignTime] = useState<number>(initTime);
+const useTime = (mode: TimerMode): ReturnType => {
+  const [remainingTime, setRemainignTime] = useState<number>(
+    calcInitTime(mode)
+  );
+
   const [isStart, setIsStart] = useState(false);
+  const [intervalId, setIntervalId] = useState(0);
 
   const startTimer = () => {
     if (!isStart) {
-      setInterval(() => {
-        setRemainignTime((t) => t - 1);
-      }, 1000);
+      const id = window.setInterval(() => {
+        setRemainignTime((t) => {
+          if (t > 0) {
+            return t - 1;
+          } else {
+            window.clearInterval(id);
+            return t;
+          }
+        });
+      }, 1);
+      setIntervalId(id);
       setIsStart(true);
     }
   };
 
-  return [remainingTime, setRemainignTime, startTimer];
+  const resetTimer = () => {
+    window.clearInterval(intervalId);
+    setIsStart(false);
+  };
+
+  const modeChange = (mode: TimerMode) => {
+    resetTimer();
+    setRemainignTime(calcInitTime(mode));
+  };
+
+  return { remainingTime, startTimer, modeChange };
 };
 
 export default useTime;
