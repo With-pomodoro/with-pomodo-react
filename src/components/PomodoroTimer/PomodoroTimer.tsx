b6eb2162917e40ca7bcc1fac3bdebaf5368d4b67
Gin2525 <gin2798@gmail.com>
2021-07-18 11:47:52 +0900

    feat: timer start button

--- src/components/PomodoroTimer/PomodoroTimer.tsx
+++ src/components/PomodoroTimer/PomodoroTimer.tsx
@@ -3,17 +3,21 @@ import styled from "styled-components";
 import { ClassNameProps } from "../../shared/types";
 import ClockBoard from "./ClockBoard";
 import useTimer from "./hooks/useTimer";
+import TimerStartButton from "./TimerStartButton";
 import { TimerMode } from "./types";
 
 const PomodoroTimer: FC<ClassNameProps> = ({ className }) => {
   const [timerMode] = useState<TimerMode>("work");
   // 秒で計算する
-  const { remainingTime, startTimer, mode } = useTimer(timerMode);
+  const { remainingTime, startTimer, mode, isStart } = useTimer(timerMode);
 
   return (
     <Container className={className}>
       <ClockBoard secOfTime={remainingTime} mode={mode} />
-      <button onClick={() => startTimer()}>start!</button>
+      <StyledTimerStartButton
+        handleClick={() => startTimer()}
+        isStart={isStart}
+      />
     </Container>
   );
 };
@@ -23,8 +27,16 @@ const Container = styled.div`
   width: 100%;
   height: 100%;
 
+  position: relative;
+
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+`;
+
+const StyledTimerStartButton = styled(TimerStartButton)`
+  position: relative;
+  top: 30px;
+  left: 160px;
 `;
