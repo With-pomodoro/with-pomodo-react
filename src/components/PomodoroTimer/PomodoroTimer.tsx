26ff3b76dd89bc93f946c62bbe97ac57a2113cfd
takaodaze <gin2798@gmail.com>
2021-07-18 09:37:46 +0900

    feat: pomodoro timer container style

--- src/components/PomodoroTimer/PomodoroTimer.tsx
+++ src/components/PomodoroTimer/PomodoroTimer.tsx
@@ -1,20 +1,30 @@
 import React, { FC, useState } from "react";
+import styled from "styled-components";
 import { ClassNameProps } from "../../shared/types";
+import ClockBoard from "./ClockBoard";
 import useTimer from "./hooks/useTimer";
 import { TimerMode } from "./types";
 
 const PomodoroTimer: FC<ClassNameProps> = ({ className }) => {
   const [timerMode] = useState<TimerMode>("work");
   // 秒で計算する
-  const { remainingTime, startTimer } = useTimer(timerMode);
+  const { remainingTime, startTimer, mode } = useTimer(timerMode);
 
   return (
-    <div className={className}>
-      <span>{remainingTime}</span>
-      <br />
+    <Container className={className}>
+      <ClockBoard secOfTime={remainingTime} mode={mode} />
       <button onClick={() => startTimer()}>start!</button>
-      <br />
-    </div>
+    </Container>
   );
 };
 export default PomodoroTimer;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
