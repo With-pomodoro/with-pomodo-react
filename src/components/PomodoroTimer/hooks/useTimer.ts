8a4f9a7547b990d39f0f58d476366a01643eaff3
takaodaze <gin2798@gmail.com>
2021-07-20 11:45:47 +0900

    refactering timer

--- src/components/PomodoroTimer/hooks/useTimer.ts
+++ src/components/PomodoroTimer/hooks/useTimer.ts
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useState } from "react";
 import { timeoutAudio } from "../../../shared/audios";
 import { TimerMode, TimerStatus } from "../types";
+import { calcLimitDate } from "../utils";
 import { calcInitTime, nextTiming } from "./utils";
 
 type ReturnType = {
@@ -13,62 +14,54 @@ type ReturnType = {
 const useTimer = (initMode: TimerMode): ReturnType => {
   const [mode, setMode] = useState<TimerMode>(initMode);
   const [status, setStatus] = useState<TimerStatus>("pause");
+  const [limitDate, setLimitDate] = useState<Date>(calcLimitDate(mode));
   const [remainingTime, setRemainignTime] = useState<number>(
     calcInitTime(initMode)
   );
+
   const [pomodoroCnt, setPomodoroCnt] = useState(0);
 
   const decreaseTime = () => {
     setRemainignTime((t) => t - 1);
   };
 
-  const modeChange = useCallback((mode: TimerMode) => {
-    resetTimer(mode);
-    setMode(mode);
-  }, []);
-
   const switchMode = useCallback(() => {
-    if (mode === "longRest") {
-      modeChange("work");
-      return;
-    }
-    if (pomodoroCnt > 0 && pomodoroCnt % 4 === 0) {
-      modeChange("longRest");
-      return;
-    }
-
-    if (mode === "work") {
-      modeChange("rest");
-    } else if (mode === "rest") {
-      modeChange("work");
-    }
-  }, [mode, modeChange, pomodoroCnt]);
-
-  useEffect(() => {
     if (mode === "work") {
       setPomodoroCnt((prev) => prev + 1);
+      setMode("rest");
+      if (pomodoroCnt > 0 && pomodoroCnt % 4 === 0) {
+        setMode("longRest");
+        return;
+      }
+    } else if (mode === "rest") {
+      setMode("work");
+    } else if (mode === "longRest") {
+      setMode("work");
     }
+  }, [mode, pomodoroCnt]);
+
+  const resetTimer = useCallback(() => {
+    setRemainignTime(calcInitTime(mode));
   }, [mode]);
 
   useEffect(() => {
-    if (status === "run" && remainingTime >= 0) {
-      setTimeout(() => {
-        decreaseTime();
-      }, nextTiming());
-    }
-  }, [status, remainingTime]);
+    resetTimer();
+  }, [mode, resetTimer]);
 
   useEffect(() => {
-    if (remainingTime < 0) {
-      timeoutAudio.play();
-      setStatus("pause");
-      switchMode();
+    if (status === "run") {
+      if (remainingTime >= 0) {
+        setTimeout(() => {
+          decreaseTime();
+        }, nextTiming());
+      } else {
+        // 残り時間が負数になった時にタイムアップの処理を行う
+        timeoutAudio.play();
+        setStatus("pause");
+        switchMode();
+      }
     }
-  }, [remainingTime, switchMode]);
-
-  const resetTimer = (mode: TimerMode) => {
-    setRemainignTime(calcInitTime(mode));
-  };
+  }, [status, remainingTime, switchMode]);
 
   const startTimer = () => {
     if (status === "pause") {
