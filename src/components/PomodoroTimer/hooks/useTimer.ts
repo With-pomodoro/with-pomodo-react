7be4cd3ed41ba255db2e0fd36cddc7017cd2d1a5
takaodaze <gin2798@gmail.com>
2021-07-20 12:59:24 +0900

    fix timer to accurate

--- src/components/PomodoroTimer/hooks/useTimer.ts
+++ src/components/PomodoroTimer/hooks/useTimer.ts
@@ -1,8 +1,7 @@
 import { useCallback, useEffect, useState } from "react";
 import { timeoutAudio } from "../../../shared/audios";
 import { TimerMode, TimerStatus } from "../types";
-import { calcLimitDate } from "../utils";
-import { calcInitTime, nextTiming } from "./utils";
+import { calcInitTime, calcLimitDate, nextTiming } from "./utils";
 
 type ReturnType = {
   remainingTime: number;
@@ -14,45 +13,45 @@ type ReturnType = {
 const useTimer = (initMode: TimerMode): ReturnType => {
   const [mode, setMode] = useState<TimerMode>(initMode);
   const [status, setStatus] = useState<TimerStatus>("pause");
-  const [limitDate, setLimitDate] = useState<Date>(calcLimitDate(mode));
+  const [limitDate, setLimitDate] = useState<Date>();
   const [remainingTime, setRemainignTime] = useState<number>(
     calcInitTime(initMode)
   );
 
   const [pomodoroCnt, setPomodoroCnt] = useState(0);
 
-  const decreaseTime = () => {
-    setRemainignTime((t) => t - 1);
-  };
+  const updateTime = useCallback(() => {
+    // setRemainignTime((t) => t - 1);
+    if (limitDate == null) return;
+
+    // 現在時刻と startTimer 関数が設定した limitDate との差を残り時間とする
+    const now = new Date();
+    const diffSec = Math.floor((limitDate.getTime() - now.getTime()) / 1000);
+    setRemainignTime(diffSec);
+  }, [limitDate]);
 
   const switchMode = useCallback(() => {
     if (mode === "work") {
       setPomodoroCnt((prev) => prev + 1);
-      setMode("rest");
       if (pomodoroCnt > 0 && pomodoroCnt % 4 === 0) {
         setMode("longRest");
-        return;
+      } else {
+        setMode("rest");
       }
-    } else if (mode === "rest") {
-      setMode("work");
-    } else if (mode === "longRest") {
+    } else if (mode === "rest" || mode === "longRest") {
       setMode("work");
     }
   }, [mode, pomodoroCnt]);
 
-  const resetTimer = useCallback(() => {
+  useEffect(() => {
     setRemainignTime(calcInitTime(mode));
   }, [mode]);
-
-  useEffect(() => {
-    resetTimer();
-  }, [mode, resetTimer]);
 
   useEffect(() => {
     if (status === "run") {
       if (remainingTime >= 0) {
         setTimeout(() => {
-          decreaseTime();
+          updateTime();
         }, nextTiming());
       } else {
         // 残り時間が負数になった時にタイムアップの処理を行う
@@ -61,11 +60,12 @@ const useTimer = (initMode: TimerMode): ReturnType => {
         switchMode();
       }
     }
-  }, [status, remainingTime, switchMode]);
+  }, [status, remainingTime, switchMode, updateTime]);
 
   const startTimer = () => {
     if (status === "pause") {
       setStatus("run");
+      setLimitDate(calcLimitDate(mode));
     }
   };
 
